074e603553cb86095a388802cdee5736cb54526f
Sina NS <400050@student.fontys.nl>
2019-11-12 21:04:06 +0100

    Taken kunnen verwijderd + voltooid worden. Voltooide taken kunnen weergegeven worden. Bepaalde controls voor de design hebben nu een naam.

--- buitengewone Takenlijst WPF/MainWindow.xaml.cs
+++ buitengewone Takenlijst WPF/MainWindow.xaml.cs
@@ -23,17 +23,18 @@ namespace buitengewone_Takenlijst_WPF
         //Lists + variabelen voor de listindex.
         List<string> huidigeTaken = new List<string>();
         List<string> voltooideTaken = new List<string>();
-        int huidigeTakenIndex = 0;
+        int huidigeTakenIndex;
 
         //Variabelen voor het aantal taken + voltooide taken.
         int huidigeTakenCount;
-        int voltooideTakenCount = 0;
+        int voltooideTakenCount;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        //Toevoegen van taken aan de 'huidigeTaken' List + weergeven in de dagen ListBox.
         private void toevoegenTaken()
         {
             string gebruikersInvoer = userInputTB.Text;
@@ -80,9 +81,135 @@ namespace buitengewone_Takenlijst_WPF
             aantalTaakLB.Content = Convert.ToString(huidigeTakenCount);
         }
 
+        //Taken verwijderen uit de ListBox + index verwijderen uit de 'huidigeTaken' List.
+        private void verwijderenTaken()
+        {
+            if (lijstMaLB.SelectedIndex > -1)
+            {
+                huidigeTaken.RemoveAt(lijstMaLB.SelectedIndex);
+                lijstMaLB.Items.Remove(lijstMaLB.SelectedItem);
+                huidigeTakenIndex--;
+                huidigeTakenCount--;
+            }
+
+            else if (lijstDiLB.SelectedIndex > -1)
+            {
+                huidigeTaken.RemoveAt(lijstDiLB.SelectedIndex);
+                lijstDiLB.Items.Remove(lijstDiLB.SelectedItem);
+                huidigeTakenIndex--;
+                huidigeTakenCount--;
+            }
+
+            else if (lijstWoLB.SelectedIndex > -1)
+            {
+                huidigeTaken.RemoveAt(lijstWoLB.SelectedIndex);
+                lijstWoLB.Items.Remove(lijstWoLB.SelectedItem);
+                huidigeTakenIndex--;
+                huidigeTakenCount--;
+            }
+
+            else if (lijstDoLB.SelectedIndex > -1)
+            {
+                huidigeTaken.RemoveAt(lijstDoLB.SelectedIndex);
+                lijstDoLB.Items.Remove(lijstDoLB.SelectedItem);
+                huidigeTakenIndex--;
+                huidigeTakenCount--;
+            }
+
+            else if (lijstVrLB.SelectedIndex > -1)
+            {
+                huidigeTaken.RemoveAt(lijstVrLB.SelectedIndex);
+                lijstVrLB.Items.Remove(lijstVrLB.SelectedItem);
+                huidigeTakenIndex--;
+                huidigeTakenCount--;
+            }
+            aantalTaakLB.Content = Convert.ToString(huidigeTakenCount);
+        }
+
+        //Taken voltooien + uit de listbox halen + toevoegen aan 'voltooideTaken' List.
+        private void voltooiTaken()
+        {
+            if (lijstMaLB.SelectedIndex > -1)
+            {
+                voltooideTaken.Add(Convert.ToString(lijstMaLB.SelectedItem));
+                lijstMaLB.Items.Remove(lijstMaLB.SelectedItem);
+                huidigeTakenCount--;
+                voltooideTakenCount++;
+            }
+
+            else if (lijstDiLB.SelectedIndex > -1)
+            {
+                voltooideTaken.Add(Convert.ToString(lijstDiLB.SelectedItem));
+                lijstDiLB.Items.Remove(lijstDiLB.SelectedItem);
+                huidigeTakenCount--;
+                voltooideTakenCount++;
+            }
+
+            else if (lijstWoLB.SelectedIndex > -1)
+            {
+                voltooideTaken.Add(Convert.ToString(lijstWoLB.SelectedItem));
+                lijstWoLB.Items.Remove(lijstWoLB.SelectedItem);
+                huidigeTakenCount--;
+                voltooideTakenCount++;
+            }
+
+            else if (lijstDoLB.SelectedIndex > -1)
+            {
+                voltooideTaken.Add(Convert.ToString(lijstDoLB.SelectedItem));
+                lijstDoLB.Items.Remove(lijstDoLB.SelectedItem);
+                huidigeTakenCount--;
+                voltooideTakenCount++;
+            }
+
+            else if (lijstVrLB.SelectedIndex > -1)
+            {
+                voltooideTaken.Add(Convert.ToString(lijstVrLB.SelectedItem));
+                lijstVrLB.Items.Remove(lijstVrLB.SelectedItem);
+                huidigeTakenCount--;
+                voltooideTakenCount++;
+            }
+            aantalTaakLB.Content = Convert.ToString(huidigeTakenCount);
+            aantalVoltooiTaakLB.Content = Convert.ToString(voltooideTakenCount);
+        }
+
+        //Voltooide taken van de 'voltooideTaken' List tonen.
+        private void toonAlleVoltooideTaken()
+        {
+            lijstVoltooiTakenLB.Items.Clear();
+
+            //Foreach Loop om alle taken uit de 'voltooideTaken' List weer te geven.
+            foreach (var afgevinkteTaken in voltooideTaken)
+            {
+                lijstVoltooiTakenLB.Items.Add(afgevinkteTaken);
+            }
+
+            //For Loop om alle taken uit de 'voltooideTaken' List weer te geven.
+            /*
+            for (int i = 0; i < voltooideTaken.Count; i++)
+            {
+                lijstVoltooiTakenLB.Items.Add(voltooideTaken.ElementAt(i));
+            }
+            */
+        }
+
         private void taakToevoegBT_Click(object sender, RoutedEventArgs e)
         {
             toevoegenTaken();
+        }
+
+        private void taakAfvink_Click(object sender, RoutedEventArgs e)
+        {
+            voltooiTaken();
+        }
+
+        private void taakVerwijder_Click(object sender, RoutedEventArgs e)
+        {
+            verwijderenTaken();
+        }
+
+        private void toonVoltooiTaken_Click(object sender, RoutedEventArgs e)
+        {
+            toonAlleVoltooideTaken();
         }
     }
 }
